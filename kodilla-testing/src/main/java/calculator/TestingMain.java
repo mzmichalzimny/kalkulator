bfb8b8da0b90fd7df8a7e7275a2d858787cb3bc6
Michał Zimny <mzmichalzimny@gmail.com>
2023-11-20 16:12:48 +0100

    6.2

---
+++ kodilla-testing/src/main/java/calculator/TestingMain.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5, 3);
+        if (addResult == 8) {
+            System.out.println("Test metody add zakończony");
+        } else {
+            System.out.println("Test metody add z błędem.");
+        }
+
+        int subtractResult = calculator.subtract(5, 3);
+        if (subtractResult == 2) {
+            System.out.println("Test metody subtract zakończony");
+        } else {
+            System.out.println("Test metody subtract z błędem.");
+        }
+    }
+}
